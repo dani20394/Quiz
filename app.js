c3ba6592d7a3dc8fe5a458b46e9b8f09b55ea0a1
Daniel de la Mata Nieves <daniel.mata.nieves@alumnos.upm.es>
2015-05-04 14:46:22 +0200

    Version css

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
+
 
 
 var app = express();
@@ -15,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -23,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 
 // catch 404 and forward to error handler
